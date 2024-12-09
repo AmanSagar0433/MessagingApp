de018807cf968bfca8f49c9e659c2b41c2739876
Anurag Sagar <anurag983sagarofficial@gmail.com>
2024-12-09 14:49:04 +0530

    Messaging app

---
+++ apps/frontend/src/components/Button.tsx
@@ -0,0 +1,11 @@
+function Button() {
+  return (
+    <>
+      <button className="w-full h-10 bg-[#0CAD3D] text-white text-sm rounded-[5px]">
+        LOGIN
+      </button>
+    </>
+  );
+}
+
+export default Button;
